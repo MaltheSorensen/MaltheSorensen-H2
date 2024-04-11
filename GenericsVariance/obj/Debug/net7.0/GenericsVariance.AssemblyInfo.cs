fbf90a6cc3a3b21d235b5b357409959fbc33b548
MaltheSorensen <malthesorensen96@gmail.com>
2024-04-11 13:17:57 +0200

    test

--- GenericsVariance/obj/Debug/net7.0/GenericsVariance.AssemblyInfo.cs
+++ GenericsVariance/obj/Debug/net7.0/GenericsVariance.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GenericsVariance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97b0518c51bb0b53c7daae20d2c14c8495bbb145")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90193a9fb9803413c2957cb5e1da691d91cae681")]
 [assembly: System.Reflection.AssemblyProductAttribute("GenericsVariance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GenericsVariance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
